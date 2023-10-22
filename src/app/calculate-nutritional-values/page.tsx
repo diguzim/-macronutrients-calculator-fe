993c3c81735141d56dbe75e885ad96dde44a2889
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-10-22 20:22:13 -0300

    Removing build errors

--- src/app/calculate-nutritional-values/page.tsx
+++ src/app/calculate-nutritional-values/page.tsx
@@ -1,10 +1,10 @@
 import _nutritionalEntities from "./nutritional-entities";
 
-export async function getNutritionalEntities() {
+async function getNutritionalEntities() {
   return Promise.resolve(_nutritionalEntities);
 }
 
-export async function CalculateNutritionalValues() {
+async function CalculateNutritionalValues() {
   const nutritionalEntities = await getNutritionalEntities();
   
   return (
