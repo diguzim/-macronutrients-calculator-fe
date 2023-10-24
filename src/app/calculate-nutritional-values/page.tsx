c9c3212aae7427a2263b0fd2369ae96dcf269d76
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-10-24 20:24:51 -0300

    WIP: Setting up dependency injection strategy through contexts

--- src/app/calculate-nutritional-values/page.tsx
+++ src/app/calculate-nutritional-values/page.tsx
@@ -1,21 +1,26 @@
+import { useContext } from "react";
 import _nutritionalEntities from "./nutritional-entities";
+import { DependencyInjectorContext } from "../../contexts/dependency-injector.provider";
 
-async function getNutritionalEntities() {
-  return Promise.resolve(_nutritionalEntities);
-}
+// async function getNutritionalEntities() {
+//   return Promise.resolve(_nutritionalEntities);
+// }
 
-async function CalculateNutritionalValues() {
-  const nutritionalEntities = await getNutritionalEntities();
+function CalculateNutritionalValues() {
+  const { listAllNutritionalEntitiesUseCase } = useContext(DependencyInjectorContext);
+  // const nutritionalEntities = await getNutritionalEntities();
+
+  console.log('listAllNutritionalEntitiesUseCase', listAllNutritionalEntitiesUseCase);
   
   return (
     <>
       <h1>Calculate nutritional values</h1>
       <ul>
-        {nutritionalEntities.map((nutritionalEntity) => (
+        {/* {nutritionalEntities.map((nutritionalEntity) => (
           <li key={`${nutritionalEntity.type}-${nutritionalEntity.id}`}>
             {nutritionalEntity.name}
           </li>
-        ))}
+        ))} */}
       </ul>
     </>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
+"use client"
 import { Box } from '@mui/material';
 import ThemeRegistry from '../components/ThemeRegistry/ThemeRegistry';
 import { Metadata } from 'next';
+import { DependencyInjectionProvider } from '../contexts/dependency-injector.provider';
 
-export const metadata: Metadata = {
-  title: 'MaCal - Macronutrients Calculator',
-  description: 'MaCal - Macronutrients Calculator',
-  keywords: 'MaCal - Macronutrients Calculator',
-}
+// export const metadata: Metadata = {
+//   title: 'MaCal - Macronutrients Calculator',
+//   description: 'MaCal - Macronutrients Calculator',
+//   keywords: 'MaCal - Macronutrients Calculator',
+// }
 
 export default function RootLayout({
   children,
@@ -16,15 +18,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ThemeRegistry options={{ key: 'mui' }}>
-          <Box component="main" sx={{
-            flexGrow: 1,
-            mx: ['16px', '120px'],
-            my: ['80px', '120px']
-          }}>
-            {children}
-          </Box>
-        </ThemeRegistry>
+        <DependencyInjectionProvider>
+          <ThemeRegistry options={{ key: 'mui' }}>
+            <Box component="main" sx={{
+              flexGrow: 1,
+              mx: ['16px', '120px'],
+              my: ['80px', '120px']
+            }}>
+              {children}
+            </Box>
+          </ThemeRegistry>
+        </DependencyInjectionProvider>
       </body>
     </html>
   )
