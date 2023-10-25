0f8d29f0dda731758a10521493808f234fa0dcfd
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-10-25 15:52:28 -0300

    Advancing usage of listAllNutritionalEntitiesUseCase within calculate nutritional values page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,34 @@
-import { Box } from '@mui/material';
-import ThemeRegistry from '../components/ThemeRegistry/ThemeRegistry';
-import { Metadata } from 'next';
-import { DependencyInjectionProvider } from '../contexts/dependency-injector.provider';
+import { Box } from "@mui/material";
+import ThemeRegistry from "../components/ThemeRegistry/ThemeRegistry";
+import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: 'MaCal - Macronutrients Calculator',
-//   description: 'MaCal - Macronutrients Calculator',
-//   keywords: 'MaCal - Macronutrients Calculator',
-// }
+export const metadata: Metadata = {
+  title: "MaCal - Macronutrients Calculator",
+  description: "MaCal - Macronutrients Calculator",
+  keywords: "MaCal - Macronutrients Calculator",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <DependencyInjectionProvider>
-          <ThemeRegistry options={{ key: 'mui' }}>
-            <Box component="main" sx={{
+        <ThemeRegistry options={{ key: "mui" }}>
+          <Box
+            component="main"
+            sx={{
               flexGrow: 1,
-              mx: ['16px', '120px'],
-              my: ['80px', '120px']
-            }}>
-              {children}
-            </Box>
-          </ThemeRegistry>
-        </DependencyInjectionProvider>
+              mx: ["16px", "120px"],
+              my: ["80px", "120px"],
+            }}
+          >
+            {children}
+          </Box>
+        </ThemeRegistry>
       </body>
     </html>
-  )
+  );
 }
