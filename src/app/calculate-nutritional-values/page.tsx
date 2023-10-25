94fe10db97b4714a9d32626937301e8945293e77
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-10-25 15:52:28 -0300

    Advancing usage of listAllNutritionalEntitiesUseCase within calculate nutritional values page

--- src/app/calculate-nutritional-values/page.tsx
+++ src/app/calculate-nutritional-values/page.tsx
@@ -1,30 +1,33 @@
-"use client"
-import { useContext } from "react";
-import _nutritionalEntities from "./nutritional-entities";
+import { useContext, useEffect, useState } from "react";
 import { DependencyInjectorContext } from "../../contexts/dependency-injector.provider";
+import { NutritionalEntity } from "../../@core/domain/nutritional-entity/nutritional-entity.entity";
 
-// async function getNutritionalEntities() {
-//   return Promise.resolve(_nutritionalEntities);
-// }
+async function CalculateNutritionalValues() {
+  // const { listAllNutritionalEntitiesUseCase } = useContext(DependencyInjectorContext);
 
-function CalculateNutritionalValues() {
-  const { listAllNutritionalEntitiesUseCase } = useContext(DependencyInjectorContext);
-  // const nutritionalEntities = await getNutritionalEntities();
+  // const [nutritionalEntities, setNutritionalEntities] = useState([] as NutritionalEntity[]);
 
-  console.log('listAllNutritionalEntitiesUseCase', listAllNutritionalEntitiesUseCase);
-  
+  // useEffect(() => {
+  //   const fetchNutritionalEntities = async () => {
+  //     const listAllNutritionalEntitiesUseCaseResult = await listAllNutritionalEntitiesUseCase.execute();
+  //     setNutritionalEntities(listAllNutritionalEntitiesUseCaseResult);
+  //   };
+
+  //   fetchNutritionalEntities();
+  // });
+
   return (
     <>
       <h1>Calculate nutritional values</h1>
-      <ul>
-        {/* {nutritionalEntities.map((nutritionalEntity) => (
+      {/* <ul>
+        {nutritionalEntities.map((nutritionalEntity) => (
           <li key={`${nutritionalEntity.type}-${nutritionalEntity.id}`}>
             {nutritionalEntity.name}
           </li>
-        ))} */}
-      </ul>
+        ))}
+      </ul> */}
     </>
-  )
+  );
 }
 
 export default CalculateNutritionalValues;
